29d75bbe36f1094d53d8c62d2a2786dd8ee1d4e7
mouadDev1 <132854935+mouadDev1@users.noreply.github.com>
2023-06-25 16:32:26 -0400

    first commit

---
+++ src/AuthPage.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+const AuthPage = (props) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target[0];
+    axios
+      .post("https://m-care-app.netlify.app/authenticate", { username: value })
+      .then((r) => props.onAuth({ ...r.data, secret: value }))
+      .catch((e) => console.log("error", e));
+  };
+
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">
+          Follow-up your Medical condition with Dr. M-Care 👨‍⚕️
+        </div>
+
+        <div className="form-subtitle">Set the username to get connected</div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input className="auth-input" name="username" />
+          <button className="auth-button" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+      <footer className="footer">
+        <p className="footer-text">
+          &copy; {new Date().getFullYear()} M-Care. Developed By{" "}
+          <a
+            className="footer-text underline"
+            href="https://mouaddev1.github.io/me/"
+          >
+            MouadDev1
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default AuthPage;
---
+++ src/ChatsPage.jsx
@@ -0,0 +1,20 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "c229fb75-c56a-4f53-9197-57648fccbd87",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
